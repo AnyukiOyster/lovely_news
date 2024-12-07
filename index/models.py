281d7e7a7b4189ac59376fb12044b78ca0d18f22
AnyukiOyster <anyukioyster@gmail.com>
2024-12-07 21:06:42 +0500

    Home, 07/12

--- index/models.py
+++ index/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Список категорий новостей.
 class NewsCategory(models.Model):
     category_name = models.CharField(max_length=64)
     category_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.category_name)
+
+# Список самих новостей.
 class News(models.Model):
     news_header = models.CharField(max_length=256)
     news_text = models.TextField()
     news_image = models.ImageField()
+    news_image_subtext = models.CharField(max_length=512)
     news_category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE)
     added_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.news_header)
--- index/views.py
+++ index/views.py
@@ -11,3 +11,21 @@ def home_page(request):
     context = {'news_list': news_list, 'categories': categories}
 
     return render(request, 'home.html', context)
+
+#Страница с новостями
+def news_page(request, pk):
+    #Вывод новости
+    news = News.objects.get(id=pk)
+    #Передаём на фронтэнд
+    context = {'news': news}
+
+    return render(request, 'news_page.html', context)
+
+#Страница категорий
+def section_page(request, pk):
+    section = NewsCategory.objects.get(id=pk)
+    news = News.objects.filter(news_category=section)
+    #Передаём на фронтэнд
+    context = {'section': section, 'news': news}
+
+    return render(request, 'section.html', context)
