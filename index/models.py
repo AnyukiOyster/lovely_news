9e799dba6e7774b14b345ac851fd81bbd72a324a
AnyukiOyster <anyukioyster@gmail.com>
2024-12-12 23:31:39 +0500

    Homework 12/12

--- index/models.py
+++ index/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 # Список категорий новостей.
 class NewsCategory(models.Model):
@@ -12,10 +13,27 @@ class NewsCategory(models.Model):
 class News(models.Model):
     news_header = models.CharField(max_length=256)
     news_text = models.TextField()
-    news_image = models.ImageField()
+    news_image = models.ImageField(upload_to='media')
+    news_mini_image = models.ImageField(upload_to='media')
     news_image_subtext = models.CharField(max_length=512)
     news_category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE)
     added_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.news_header)
+
+
+# Генерация случайных новостей для главной
+def random_news():
+    news_all = News.objects.all()
+    amount_news = int(len(news_all) +1)
+    i = 1
+    while i == 1:
+        random_num1 = random.randrange(1, amount_news, 1)
+        random_num2 = random.randrange(1, amount_news, 1)
+        if random_num1 == random_num2:
+            i = 1
+        else:
+            i = 0
+            return random_num1, random_num2
+
--- index/views.py
+++ index/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import NewsCategory, News
+from .models import NewsCategory, News, random_news
+
 
 # Create your views here.
 #Главная страница
@@ -7,8 +8,17 @@ def home_page(request):
     #Достаём данные из БД
     news_list = News.objects.all()
     categories = NewsCategory.objects.all()
+    nums = random_news()
+    num1 = nums[0]
+    num2 = nums[1]
+    article1 = News.objects.get(id=num1)
+    article2 = News.objects.get(id=num2)
+    news_pool = []
+    for a in news_list[::-1]:
+        news_pool.append(a)
+
     #Передаём данные на FE
-    context = {'news_list': news_list, 'categories': categories}
+    context = {'news_list': news_list, 'categories': categories,'news_pool': news_pool, 'article1': article1, 'article2': article2}
 
     return render(request, 'home.html', context)
 
@@ -16,16 +26,18 @@ def home_page(request):
 def news_page(request, pk):
     #Вывод новости
     news = News.objects.get(id=pk)
+    categories = NewsCategory.objects.all()
     #Передаём на фронтэнд
-    context = {'news': news}
+    context = {'news': news, 'categories': categories}
 
     return render(request, 'news_page.html', context)
 
 #Страница категорий
 def section_page(request, pk):
-    section = NewsCategory.objects.get(id=pk)
-    news = News.objects.filter(news_category=section)
+    category = NewsCategory.objects.get(id=pk)
+    categories = NewsCategory.objects.all()
+    news = News.objects.filter(news_category=category)[::-1]
     #Передаём на фронтэнд
-    context = {'section': section, 'news': news}
+    context = {'category': category, 'categories': categories, 'news': news}
 
     return render(request, 'section.html', context)
